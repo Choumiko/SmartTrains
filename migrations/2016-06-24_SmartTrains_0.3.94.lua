33ccb250b41991b6368d146a827e4622a66bdee5
Choumiko <ghostface@ghostface.info>
2020-11-25 11:52:25 +0100

    minor

--- migrations/2016-06-24_SmartTrains_0.3.94.lua
+++
@@ -1,7 +0,0 @@
-for _, force in pairs(game.forces) do
-  local techs = force.technologies
-  local recipes = force.recipes
-  if techs["automated-rail-transportation"].researched then
-    recipes["smart-train-stop"].enabled = true
-  end
-end
